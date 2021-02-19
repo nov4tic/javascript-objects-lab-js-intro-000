34e9f1ea49eab11e29ecd76f0ea05e047181a496
Dev Frias <ddfrias14@gmail.com>
2021-02-19 16:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var recipes = {Eggs: 3}
 
 var updateObjectWithKeyAndValue = (obj, key, value) => Object.assign({}, obj, {[key]: value});
 
-var destructivelyUpdateObjectWithKeyAndValue = (obj, key, value) => obj[key] = value;
+var destructivelyUpdateObjectWithKeyAndValue = (obj, key, value) => obj.[key] = value;
