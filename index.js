1b002923a4ee9ef8636c7e20c1bd318cc5d15ba2
Dev Frias <ddfrias14@gmail.com>
2021-02-19 16:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var recipes = {Eggs: 3}
 
-var updateObjectWithKeyAndValue = (obj, key, value) => Object.assign({}, obj, {key: value});
+var updateObjectWithKeyAndValue = (obj, key, value) => Object.assign({}, obj, {[key]: value});
