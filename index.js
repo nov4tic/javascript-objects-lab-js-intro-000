ca400e3e4051c28ebd5de5e7cf2c7034cb8d1acb
Dev Frias <ddfrias14@gmail.com>
2021-02-19 16:52:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var recipes = {Eggs: 3}
 var updateObjectWithKeyAndValue = (obj, key, value) => Object.assign({}, obj, {[key]: value});
 
 var destructivelyUpdateObjectWithKeyAndValue = (obj, key, value) => Object.assign(obj, {[key]: value});
+
+var deleteFromObjectByKey = (obj, key) => {
+  var newObj = Object.assign({}, obj);
+  delete newObj[key]l
+  return newObj;
+}
